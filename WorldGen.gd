541dfd46b847064efb037eeee8dea58b2f283c06
Flamtky <n.schumann@rz-online.de>
2023-07-21 17:25:57 +0200

    refactored WorldGen Script

--- WorldGen.gd
+++ WorldGen.gd
@@ -1,4 +1,4 @@
-extends TileMap
+extends Node2D
 
 var target_tile_id = 1
 var cell_size = Vector2(128, 128)
@@ -6,6 +6,8 @@ var cell_size = Vector2(128, 128)
 @export var WORLD_SIZE:int = 25
 @export var BUILDINGS = ["res://assets/buildings/skyscraper/skyscraper1new.png"]
 @export var OFFSET = 24
+@onready var Buildings = get_node("Buildings")
+@onready var WorldTileMap = get_node("WorldTileMap")
 
 func _ready():
     randomize()  # initialize the random seed
@@ -16,8 +18,7 @@ func generate_tiles():
     for x in range(-size, size):
         for y in range(-size, size):
             var tile_id = randi() % 2
-            print(tile_id)
-            set_cell(0, Vector2(x, y), 0, Vector2(tile_id, 0)) # TODO: Change later to autotile
+            WorldTileMap.set_cell(0, Vector2(x, y), 0, Vector2(tile_id, 0)) # TODO: Change later to autotile
             if tile_id == target_tile_id:
                 place_building_at_tile(Vector2(x,y))
 
@@ -25,8 +26,7 @@ func place_building_at_tile(tileCords):
     var sprite = Sprite2D.new()
     var random_choice = BUILDINGS[randi() % BUILDINGS.size()]
     sprite.texture = load(random_choice)
-    sprite.position = to_global(map_to_local(tileCords))
+    sprite.position = WorldTileMap.to_global(WorldTileMap.map_to_local(tileCords))
     sprite.position.y -= sprite.texture.get_height() / 2 - OFFSET # TODO: Check why the offset
-    var buildings_node = %Buildings
-    buildings_node.call_deferred("add_child", sprite)
+    Buildings.call_deferred("add_child", sprite)
 
