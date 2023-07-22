4704419722978749d937c128e2adeaa518f60d45
Flamtky <n.schumann@rz-online.de>
2023-07-22 18:11:24 +0200

    Refactored whole worldgen
    - now using methods to prevent code duclipation and more readability
    - error handling
    - new nicer exports to better control the generator

---
+++ core/World/WorldGen.gd
@@ -0,0 +1,176 @@
+extends Node2D
+
+@export_group("World")
+@export var WORLD_SIZE:int = 200
+@export var BUILDINGS: Array[PackedScene]
+@export var BUILDING_OFFSET = 24
+@export var BUILDING_TINT = Color(0.0, 0.0, 0.2, 1.0)
+
+@export_group("Noise")
+@export var SEED: int = randi()
+@export var FREQUENCY: float = 0.2
+@export var OCTAVES: int = 3
+@export var GAIN: float = 0.1
+@export var LACUNARITY: float = 0.1
+
+@export_group("Debug")
+@export var SHOW_NOISE: bool = false
+
+@onready var Buildings = $Buildings
+@onready var WorldTileMap = $WorldTileMap
+
+const target_tile_id = 1
+var randomGen = RandomNumberGenerator.new()
+var noodleNoise
+
+# Function to initialize noise parameters
+func _ready():
+    var base_noise = await setup_noise(SEED, FREQUENCY, OCTAVES, GAIN, LACUNARITY)
+    generate_and_dilate_noise(base_noise)
+
+    if SHOW_NOISE:
+        $Noise.texture = ImageTexture.create_from_image(noodleNoise)
+    generate_tiles()
+
+# Function to setup noise parameters
+func setup_noise(noise_seed, frequency, octaves, noise_gain, lacunarity) -> Image:
+    randomGen.seed = noise_seed
+
+    var noise = FastNoiseLite.new()
+    noise.set_noise_type(FastNoiseLite.TYPE_PERLIN)
+    noise.set_seed(noise_seed)
+    noise.set_frequency(frequency)
+    noise.set_fractal_octaves(octaves)
+    noise.set_fractal_gain(noise_gain)
+    noise.set_fractal_lacunarity(lacunarity)
+    
+    # Generate noise texture
+    var noiseTexture = NoiseTexture2D.new()
+    noiseTexture.noise = noise
+    noiseTexture.width = WORLD_SIZE
+    noiseTexture.height = WORLD_SIZE
+    await noiseTexture.changed
+
+    return noiseTexture.get_image()
+
+# Function to generate and dilate noise
+func generate_and_dilate_noise(base_noise):
+    var image = create_grid_noise()
+    apply_noise_to_grid(base_noise, image)
+    dilate(image, Color.WHITE, 2)
+    dilate(image, Color.WEB_GRAY, 1, Color.WHITE, true)
+    var borderStreets = get_streets_on_border(image)
+
+    print("streets_on_border: " + str(borderStreets.size()))
+    set_meta("streets_on_border", borderStreets)
+
+    noodleNoise = image
+
+# Function to create grid noise
+func create_grid_noise() -> Image:
+    var image = Image.create(WORLD_SIZE, WORLD_SIZE, false, Image.FORMAT_RGBA8)
+    
+    for x in range(WORLD_SIZE):
+        for y in range(WORLD_SIZE):
+            if x % 2 != 0 and y % 2 != 0:
+                image.set_pixel(x, y, Color.WHITE) # House
+            else:
+                image.set_pixel(x, y, Color.BLACK) # Nothing
+    return image
+
+# Function to apply noise to the grid
+func apply_noise_to_grid(noiseImg, image):
+    for x in range(WORLD_SIZE):
+        for y in range(WORLD_SIZE):
+            var value = noiseImg.get_pixel(x, y).r
+            if value > 0.8:
+                image.set_pixel(x, y, Color.WHITE) # House
+            else:
+                image.set_pixel(x, y, Color.BLACK) # Nothing
+
+# Function to dilate the noise
+func dilate(image, target_color: Color, iterations=1, check_color=null, check_diagonals=false):
+    for _i in range(iterations):
+        var copyImg = image.duplicate()
+        for x in range(WORLD_SIZE):
+            for y in range(WORLD_SIZE):
+                if getPixel(copyImg, x, y) != Color.WHITE:
+                    continue
+                if check_color == null or getPixel(copyImg, x - 1, y) != check_color:
+                    setPixel(image, x - 1, y, target_color)
+                if check_color == null or getPixel(copyImg, x + 1, y) != check_color:
+                    setPixel(image, x + 1, y, target_color)
+                if check_color == null or getPixel(copyImg, x, y - 1) != check_color:
+                    setPixel(image, x, y - 1, target_color)
+                if check_color == null or getPixel(copyImg, x, y + 1) != check_color:
+                    setPixel(image, x, y + 1, target_color)
+
+                # Diagonals
+                if !check_diagonals:
+                    continue
+                if check_color == null or getPixel(copyImg, x - 1, y - 1) != check_color:
+                    setPixel(image, x - 1, y - 1, target_color)
+                if check_color == null or getPixel(copyImg, x + 1, y - 1) != check_color:
+                    setPixel(image, x + 1, y - 1, target_color)
+                if check_color == null or getPixel(copyImg, x - 1, y + 1) != check_color:
+                    setPixel(image, x - 1, y + 1, target_color)
+                if check_color == null or getPixel(copyImg, x + 1, y + 1) != check_color:
+                    setPixel(image, x + 1, y + 1, target_color)
+
+func setPixel(image, x, y, color):
+    if x < 0 or x >= image.get_width() or y < 0 or y >= image.get_height():
+        return
+    image.set_pixel(x, y, color)
+
+func getPixel(image, x, y):
+    if x < 0 or x >= image.get_width() or y < 0 or y >= image.get_height():
+        return Color.BLACK
+    return image.get_pixel(x, y)
+
+# Function to get target_color on the borderStreets
+func get_streets_on_border(image):
+    var borderStreets = []
+    for x in range(WORLD_SIZE):
+        if abs(image.get_pixel(x, 0).r - 0.5) < 0.1:
+            borderStreets.append(Vector2(x, 0))
+        if abs(image.get_pixel(x, WORLD_SIZE - 1).r - 0.5) < 0.1:
+            borderStreets.append(Vector2(x, WORLD_SIZE - 1))
+        if abs(image.get_pixel(0, x).r - 0.5) < 0.1:
+            borderStreets.append(Vector2(0, x))
+        if abs(image.get_pixel(WORLD_SIZE - 1, x).r - 0.5) < 0.1:
+            borderStreets.append(Vector2(WORLD_SIZE - 1, x))
+    return borderStreets
+
+# Function to generate tiles
+func generate_tiles():
+    var size = WORLD_SIZE/2
+    var streets = []
+
+    for x in range(-size, size):
+        for y in range(-size, size):
+            var pixel = noodleNoise.get_pixel(x + size, y + size).r
+            if pixel == 1.0:
+                place_building_at_tile(Vector2(x, y))
+                WorldTileMap.set_cell(1, Vector2(x, y), 0, Vector2(3, 3))
+            elif pixel > 0.0:
+                streets.append(Vector2(x, y))
+            else:
+                WorldTileMap.set_cell(0, Vector2(x, y), 1, Vector2(0, 0))
+                
+    WorldTileMap.set_cells_terrain_connect(0, streets, 0, 0)
+
+# Function to place building at tile
+func place_building_at_tile(tileCords):
+    var random_choice = BUILDINGS[randomGen.randi_range(0, BUILDINGS.size() - 1)]
+    var building = random_choice.instantiate()
+    var sprite = building.get_node("Sprite2D")
+
+    # random tint of base_color for the sprite
+    var colorOff = randomGen.randf()
+    var tintColor = BUILDING_TINT + Color(colorOff, colorOff, colorOff, 0.0)
+    sprite.modulate = tintColor
+
+    sprite.position = WorldTileMap.to_global(WorldTileMap.map_to_local(tileCords))
+    sprite.position.y -= sprite.texture.get_height() / 2 - BUILDING_OFFSET
+
+    Buildings.add_child(building)
